d30e6313bebeebf68bd2537249da3d6c27e737b2
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-10 22:28:29 +0100

    Added forbidden view to timeseries site.

--- dataox/urls/timeseries.py
+++ dataox/urls/timeseries.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from humfrey.misc.views import SimpleView
 urlpatterns = patterns('',
     (r'^', include('openorg_timeseries.urls', 'timeseries')),
+    (r'^forbidden/$', SimpleView.as_view(template_name='forbidden', context={'status_code': 403}), {}, 'forbidden'),
 )
 
 handler404 = SimpleView.as_view(template_name='404-main', context={'status_code':404})
