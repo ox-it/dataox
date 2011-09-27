55d2b8b5f951e65326c54a2c10e2ff11b1b2a956
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2011-09-27 10:32:28 +0100

    Added test settings for dataox, so we can run dataox specific tests, too.

---
+++ dataox/tests/settings.py
@@ -0,0 +1,23 @@
+import os
+
+os.environ['HUMFREY_CONFIG_FILE'] = os.path.join(os.path.dirname(__file__), 'data', 'config.ini')
+
+from dataox.settings import *
+
+INSTALLED_APPS += (
+    'django_jenkins',
+)
+
+JENKINS_TEST_RUNNER = 'humfrey.tests.HumfreyJenkinsTestSuiteRunner'
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                 'django_jenkins.tasks.with_coverage',
+                 'django_jenkins.tasks.django_tests',
+                 'django_jenkins.tasks.run_pep8',
+                 'django_jenkins.tasks.run_pyflakes')
+PROJECT_APPS = tuple(app for app in INSTALLED_APPS if app.startswith('humfrey.'))
+
+TEST_URI = 'http://data.example.com/id/Foo'
+TEST_DOMAIN = 'data.ox.ac.uk'
+
+EXTRA_TEST_MODULES = ('dataox.datasets.vacancies.tests',
+                      )
