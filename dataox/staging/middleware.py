6f2fa8e5d740995e31c5dbaf2cb342b9d84d8d54
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-12 15:04:03 +0100

    CSRF protection checks referer; we'll need to mangle that when running from one domain.

--- dataox/staging/middleware.py
+++ dataox/staging/middleware.py
@@ -1,4 +1,5 @@
 import re
+import urlparse
 
 from django_hosts import middleware
 
@@ -12,6 +13,20 @@ class StagingMiddleware(object):
         request.original_http_host = request.META['HTTP_HOST']
         request.META['HTTP_HOST'] = path_info['host']
         request.path_info = path_info['path']
+
+        if 'HTTP_REFERER' in request.META:
+            referer = urlparse.urlparse(request.META['HTTP_REFERER'])
+            path_info = self.path_info_re.match(referer.path)
+            if referer.netloc != request.original_http_host or not path_info:
+                return
+            path_info = path_info.groupdict()
+
+            request.META['HTTP_REFERER'] = urlparse.urlunparse((referer.scheme,
+                                                                path_info['host'],
+                                                                path_info['path'],
+                                                                referer.params,
+                                                                referer.query,
+                                                                referer.fragment))
 
     def process_response(self, request, response):
         if hasattr(request, 'original_http_host'):
