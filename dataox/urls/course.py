b447e6900fcf81b7c146936e07f4b6a3cf6df946
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-18 15:45:05 +0100

    Removed unnecessary imports from dataox.urls.course.

--- dataox/urls/course.py
+++ dataox/urls/course.py
@@ -1,10 +1,8 @@
 from django.conf.urls.defaults import patterns, url, include
-from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from dataox.equipment import views as equipment_views
-
 from humfrey.misc.views import SimpleView
+
 urlpatterns = patterns('',
     url(r'^', include('dataox.course.urls', 'course')),
 ) + staticfiles_urlpatterns()
