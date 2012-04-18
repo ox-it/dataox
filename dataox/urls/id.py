250e8518ce089450d8ba0aae6c03909b1f4b8bbb
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-18 16:24:01 +0100

    Added missing module.

---
+++ dataox/urls/id.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from humfrey.desc import views as desc_views
+from humfrey.misc.views import SimpleView
+
+urlpatterns = patterns('',
+    url(r'^.*', desc_views.IdView.as_view(), {}, 'id'),
+) + staticfiles_urlpatterns()
+
+handler404 = SimpleView.as_view(template_name='404-main', context={'status_code':404})
+handler500 = SimpleView.as_view(template_name='500', context={'status_code':500})
