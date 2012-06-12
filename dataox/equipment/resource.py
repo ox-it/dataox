ba605ef9c994fa7aa2386510a92b3a921d25a0a8
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-06-12 13:45:16 +0100

    Added equipment and equipmentOf properties to equipment resources.

--- dataox/equipment/resource.py
+++ dataox/equipment/resource.py
@@ -15,6 +15,7 @@ class Equipment(object):
             '%(uri)s dcterms:subject %(category)s',
             
             '%(uri)s oo:equipmentOf %(department)s',
+            '%(uri)s oo:organizationPart %(department)s',
             '%(uri)s foaf:based_near %(basedNear)s',
             '%(uri)s spatialrelations:within %(within)s',
             '%(uri)s gr:hasMakeAndModel %(makeAndModel)s . %(makeAndModel)s gr:hasManufacturer %(manufacturer)s',
@@ -32,12 +33,31 @@ class Equipment(object):
     def geo_long(self):
         return self.geo_provider.geo_long if self.geo_provider else None
 
+    @property
+    def equipmentOf(self):
+        return ( self.get('oo:equipmentOf') or self.get('oo:organizationPart') )
+
+
 class Organization(dataox.resource.oxpoints.Organization):
     @classmethod
     def _describe_patterns(cls):
         return [
             '%(equipment)s oo:equipmentOf %(uri)s',
+            #'%(equipment)s oo:organizationPart %(uri)s',
+            #'%(equipment)s oo:accessPrerequisite %(accessPrerequisite)s',
+            #'%(equipment)s oo:useRestriction %(useRestriction)s',
+            #'%(equipment)s oo:availability %(availability)s',
+            #'%(equipment)s dcterms:subject %(category)s',
         ]
+
+    @property
+    def allEquipment(self):
+        try:
+            return self.get_all('oo:equipmentOf', inverse=True) + self.get_all('oo:organizationPart', inverse=True)
+        except:
+            import traceback
+            traceback.print_exc()
+            raise
 
 resource_registry = dataox.resource.resource_registry + ResourceRegistry(
     Equipment, Organization
