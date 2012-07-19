fd48742564c8151254f9a934b30d43886eb3128c
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-07-19 13:58:56 +0100

    Added 404 and 500 handlers to staging urls.

--- dataox/staging/urls.py
+++ dataox/staging/urls.py
@@ -5,3 +5,6 @@ from humfrey.misc import views as misc_views
 urlpatterns = patterns('',
     url(r'^$', misc_views.SimpleView.as_view(template_name='staging/index')),
 )
+
+handler404 = misc_views.SimpleView.as_view(template_name='404', context={'status_code':404})
+handler500 = misc_views.SimpleView.as_view(template_name='500', context={'status_code':500})
