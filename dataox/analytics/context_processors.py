bf89f1e585901888f25898641813467e17ecf2fb
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2012-10-05 18:06:40 +0100

    Fixed analytics for hosts other than 'data' (missing url pattern).

--- dataox/analytics/context_processors.py
+++ dataox/analytics/context_processors.py
@@ -5,6 +5,8 @@ We use the IT Services "optional analytics" JavaScript documented at
 http://infodev.oucs.ox.ac.uk/analytics/.
 """
 
+from django_hosts import reverse_full
+
 # hostname will be used with the _setDomainName Google Analytics option; see:
 # <https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApiDomainDirectory#_gat.GA_Tracker_._setDomainName>
 # login_possible determines whether users will be told that the policy will
@@ -12,11 +14,13 @@ http://infodev.oucs.ox.ac.uk/analytics/.
 analytics_meta = [{'analytics_id': 'UA-32168758-1',
                    'hosts': ('equipment',),
                    'hostname': 'www.research-facilities.ox.ac.uk',
-                   'login_possible': True},
+                   'login_possible': True,
+                   'privacy_policy_url': reverse_full('equipment', 'legal') + '#analytics'},
                   {'analytics_id': 'UA-35258720-1',
                    'hosts': ('data', 'course', 'timeseries', 'empty'),
                    'hostname': '.data.ox.ac.uk',
-                   'login_possible': False}]
+                   'login_possible': False,
+                   'privacy_policy_url': reverse_full('data', 'legal') + '#analytics'}]
 
 analytics_meta_by_host = dict((host, meta) for meta in analytics_meta
                                            for host in meta['hosts'])
@@ -28,6 +32,7 @@ def do_not_track(request):
         return {'analytics': {'do_not_track': request.META.get('HTTP_DNT') == '1',
                               'id': meta['analytics_id'],
                               'hostname': meta['hostname'],
-                              'login_possible': meta['login_possible']}}
+                              'login_possible': meta['login_possible'],
+                              'privacy_policy_url': meta['privacy_policy_url']}}
     else:
         return {}
