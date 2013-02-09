55ec4dd7a9c3043b2e8b97a6aa51fa302bdc5321
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-02-09 12:13:40 +0000

    institution and based near fields in equipment shouldn't be required

--- dataox/equipment/forms.py
+++ dataox/equipment/forms.py
@@ -12,27 +12,29 @@ def AdvancedSearchForm(*args, **kwargs):
     q = {'size': 0,
          'facets': {'formalOrganisation': {'terms': {'field': 'formalOrganisation.uri'}},
                     'basedNear': {'terms': {'field': 'basedNear.uri'}}}}
-    
+
     results = json.load(urllib2.urlopen(search_url, json.dumps(q)))
-    
+
     formal_organisation_choices = [t['term'] for t in results['facets']['formalOrganisation']['terms']]
     based_near_choices = [t['term'] for t in results['facets']['basedNear']['terms']]
-    
+
     labels = get_labels(map(rdflib.URIRef, formal_organisation_choices + based_near_choices),
                         endpoint=store.query_endpoint)
-    
+
     formal_organisation_choices = [('', '-'*20)]+[(uri, labels.get(rdflib.URIRef(uri), uri)) for uri in formal_organisation_choices]
     based_near_choices = [('', '-'*20)]+[(uri, labels.get(rdflib.URIRef(uri), uri)) for uri in based_near_choices]
 
     attrs = {'q': forms.CharField(label='Query'),
              'filter.basedNear.uri': forms.ChoiceField(label='Location',
                                                        choices=based_near_choices,
-                                                       initial=''),
+                                                       initial='',
+                                                       required=False),
              'filter.formalOrganisation.uri': forms.ChoiceField(label='Institution',
                                                                 choices=formal_organisation_choices,
-                                                                initial='http://oxpoints.oucs.ox.ac.uk/id/00000000')}
+                                                                initial='http://oxpoints.oucs.ox.ac.uk/id/00000000',
+                                                                required=False)}
     form = type('AdvancedSearchForm', (forms.Form,), attrs)
-    
+
     return form(*args, **kwargs)
 
 class ContributeForm(forms.Form):
