939ac866159ebcccec03db5af06a65009bc58aa5
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-03-25 16:22:42 +0000

    Properly fixed login/logout URLs in backstage.

--- dataox/urls/backstage.py
+++ dataox/urls/backstage.py
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
     url(r'^stores/', include('humfrey.sparql.urls.admin', 'sparql-admin')),
 
     url(r'^account/login/$', auth_views.login, name='account_login'),
-    url(r'^account/webauth/$', django_webauth.views.LoginView.as_view(), name='webauth_login'),
-    url(r"^account/logout/$", django_webauth.views.LogoutView.as_view(), name="webauth_logout"),
+    url(r'^account/webauth/$', django_webauth.views.LoginView.as_view(), name='account_webauth'),
+    url(r"^account/logout/$", django_webauth.views.LogoutView.as_view(), name="account_logout"),
 
     url(r'^oauth2/', include('dataox.oauth2.urls', 'oauth2')),
     url(r'^admin/', admin.site.urls),
