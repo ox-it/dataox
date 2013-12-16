877b5d67705247f1ce34dcec8b88641ccd5c33a5
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-12-16 13:38:08 +0000

    Added IdView that works across hosts

--- dataox/urls/id.py
+++ dataox/urls/id.py
@@ -1,13 +1,22 @@
 from django.conf.urls.defaults import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django_hosts import reverse_full
 
 from humfrey.desc import views as desc_views
 from humfrey.misc import views as misc_views
 
 from .main import handler500
 
+class IdView(desc_views.IdView):
+    @property
+    def doc_view(self):
+        return reverse_full('data', 'doc-generic')
+    @property
+    def desc_view(self):
+        return reverse_full('data', 'desc')
+
 urlpatterns = patterns('',
-    url(r'^.*', desc_views.IdView.as_view(), {}, 'id'),
+    url(r'^.*', IdView.as_view(), {}, 'id'),
 ) + staticfiles_urlpatterns()
 
 handler404 = misc_views.SimpleView.as_view(template_name='404-id', context={'status_code':404})
