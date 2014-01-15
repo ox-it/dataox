5c760b36a553ac8482d46776d094a1d405577911
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-01-15 16:14:37 +0000

    Support '-' in format names, as it's needed for json-ld and rdf-json.

--- dataox/old_feeds/urls.py
+++ dataox/old_feeds/urls.py
@@ -6,9 +6,9 @@ urlpatterns = patterns('',
     (r'^$', views.IndexView.as_view(), {}, 'index'),
     (r'^vacancies/$', views.VacancyIndexView.as_view(), {}, 'vacancies-index'),
     # Units
-    (r'^vacancies/(?P<oxpoints_id>\d{8})(?:\.(?P<format>[\da-z]+))?$', views.VacancyView.as_view(), {}, 'vacancies'),
+    (r'^vacancies/(?P<oxpoints_id>\d{8})(?:\.(?P<format>[\da-z\-]+))?$', views.VacancyView.as_view(), {}, 'vacancies'),
     # Everything
     (r'^vacancies/all(?:\.(?P<format>[\da-z]+))?$', views.VacancyView.as_view(), {}, 'vacancies-all'),
     # Unit and subunits
-    (r'^all-vacancies/(?P<oxpoints_id>\d{8})(?:\.(?P<format>[\da-z]+))?$', views.VacancyView.as_view(all=True), {}, 'all-vacancies'),
+    (r'^all-vacancies/(?P<oxpoints_id>\d{8})(?:\.(?P<format>[\da-z\-]+))?$', views.VacancyView.as_view(all=True), {}, 'all-vacancies'),
 )
