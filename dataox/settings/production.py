8a6145d3aea27c220910c5ec9361154ce7eac5c6
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2015-04-22 10:58:46 +0100

    Add ALLOWED_HOSTS

--- dataox/settings/production.py
+++ dataox/settings/production.py
@@ -2,3 +2,12 @@ from .base import *
 
 DEBUG = False
 STAGING = False
+
+ALLOWED_HOSTS = [
+    'data.ox.ac.uk',
+    'backstage.data.ox.ac.uk',
+    'static.data.ox.ac.uk',
+    'id.it.ox.ac.uk',
+    'course.data.ox.ac.uk',
+    'www.research-facilities.ox.ac.uk',
+]
